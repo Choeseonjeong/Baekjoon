dc38cc4ba99afbc71d8e3b07a85e16fa9f7ab84e
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2024-09-08 12:58:32 +0900

    [level 1] Title: 음양 더하기, Time: 0.07 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -0,0 +1,8 @@
+def solution(absolutes, signs):
+    answer=0
+    for absolutes, signs in zip(absolutes, signs):
+        if signs==False:
+            answer-=absolutes
+        else:
+            answer+=absolutes
+    return answer
