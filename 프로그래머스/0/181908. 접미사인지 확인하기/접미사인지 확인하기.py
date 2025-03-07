4d1f1f68ed4130a624b223551c0f8d7427432ed3
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-03-07 18:31:59 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
@@ -1,8 +1,5 @@
-def solution(my_string, is_prefix):
-    arr=[]
+def solution(my_string, is_suffix):
+    arr = []
     for i in range(len(my_string)):
         arr.append(my_string[i:])
-    if is_prefix in arr:
-        return 1
-    else:
-        return 0
+    return int(is_suffix in arr)
