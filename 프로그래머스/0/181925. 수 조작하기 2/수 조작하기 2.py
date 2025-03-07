29c429dfc9f5577c96e9c91a4203da2554126237
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-03-07 16:03:35 +0900

    [level 0] Title: 수 조작하기 2, Time: 19.88 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
@@ -1,12 +1,8 @@
 def solution(numLog):
-    operation = {1: "w", -1: "s", 10: "d", -10: "a"}
-    result = []
-
+    arr = {1: "w", -1: "s", 10: "d", -10: "a"}
+    answer = ''
     for i in range(1, len(numLog)):
         diff = numLog[i] - numLog[i-1]
-        if diff in operation:
-            result.append(operation[diff])
-
-    return "".join(result)
-
-
+        if diff in arr:
+            answer += arr[diff]
+    return answer
