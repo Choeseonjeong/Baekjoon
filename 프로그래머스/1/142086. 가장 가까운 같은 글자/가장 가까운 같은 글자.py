b73c470409fd7675b3c44c7146141896c9fd16a6
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-03-24 16:06:47 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 3.67 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -1,10 +1,12 @@
 def solution(s):
-    answer = []
-    dic = dict()
+    answer = []    
+    dic = {}
     for i in range(len(s)):
-        if s[i] not in dic:
-            answer.append(-1)
+        if s[i] in dic:
+            answer.append(i - dic[s[i]])
+            dic[s[i]] = i
         else:
-            answer.append(i-dic[s[i]])
-        dic[s[i]]=i
+            answer.append(-1)
+            dic[s[i]] = i
+    
     return answer
