462dca2ac798df708015c98a86e869177253b48c
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-03-25 19:59:04 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.01 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.py
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.py
@@ -1,12 +1,5 @@
 def solution(arr, idx):
-    answer = arr[idx:]
-    num = 0
-    count = answer.count(1)
-    if count==0:
-        return -1 
-    else:
-        for i in answer:
-            if i != 1:
-                num+=1
-            else:
-                return idx+num
+    for i in range(idx, len(arr)):
+        if arr[i] == 1:
+            return i
+    return -1
