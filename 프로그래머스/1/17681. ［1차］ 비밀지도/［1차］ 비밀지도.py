c34ac48420b1c4dcb2d6aae0632ac1b2d7c63b68
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-04-01 12:53:22 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.06 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,8 +1,25 @@
 def solution(n, arr1, arr2):
-    cnt=[]
-    for i in range((n)):
-        num = (bin((arr1[i])|(arr2[i]))[2:]) # 비트연산자 : |
-        num = num.zfill(n)
-        result = num.replace("1","#").replace("0"," ")
-        cnt.append(result)
-    return cnt
+    def twoDemical(arr):
+        num1 = []
+        for i in arr:
+            a = bin(i)[2:]
+            a = '0' * (n - len(a)) + a
+            num1.append(list(a))  
+        return num1
+    
+    num1 = twoDemical(arr1)
+    num2 = twoDemical(arr2)
+    
+    result = []
+    for i in range(n):
+        temp = ""
+        for j in range(n):
+            if num1[i][j] == "1" or num2[i][j] == "1":
+                temp += "#"
+            else:
+                temp += " "
+        result.append(temp)
+    return result
+
+
+
