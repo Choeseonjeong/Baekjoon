9586eec2ddcaeac2fb016061452b37ac77f4d40e
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-04-22 13:35:52 +0900

    [Bronze II] Title: 소수, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.py
+++ 백준/Bronze/2581. 소수/소수.py
@@ -1,25 +1,20 @@
 def is_prime(n):
     if n < 2:
-        return 0
+        return False
     for i in range(2,int(n**0.5)+1):
         if n % i == 0:
-            return 0
-    return n
+            return False
+    return True
 
 n = int(input())
 m = int(input())
-answer = []
-result = []
+answer = [i for i in range(n,m+1) if is_prime(i)]
 
-for i in range(n,m+1):
-    answer.append(is_prime(i))
-for i in range(len(answer)):
-    if answer[i] != 0:
-        result.append(answer[i])
 
-if len(result)==0:
+
+if len(answer)==0:
     print(-1)
 else:
-    print(sum(result))
-    print(result[0])
+    print(sum(answer))
+    print(answer[0])
 
