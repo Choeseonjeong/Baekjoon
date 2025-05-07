effd10ca5c055b98cf39560d008ceec083e61ce9
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-05-07 18:53:25 +0900

    [Silver V] Title: 숫자 카드, Time: 736 ms, Memory: 143456 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,17 +1,19 @@
 import sys
 
-n = sys.stdin.readline()
-arr1 = list(map(int,input().split()))
-n = sys.stdin.readline()
-arr2 = list(map(int,input().split()))
-dic = {}
-for i in arr2:
-    dic[i]=0
-    
+N = int(sys.stdin.readline())
+arr1 = set(map(int,sys.stdin.readline().split()))
+
+M = int(sys.stdin.readline())
+
+dic = dict()
+
 for i in arr1:
-    if i in dic:
-        dic[i]=1
-        
+    dic[i] = 0
     
-for i in dic:
-    print(dic[i], end=' ')
+arr2 = list(map(int,sys.stdin.readline().split()))
+
+for i in arr2:
+    if i in dic:
+        print(1,end=' ')
+    else:
+        print(0,end=' ')
