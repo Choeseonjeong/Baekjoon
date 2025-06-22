66a963b3a324a92a7f19e886d5f9eaa2610462ce
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-06-22 18:41:27 +0900

    [level 2] Title: 카펫, Time: 0.21 ms, Memory: 9.04 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,8 +1,8 @@
 def solution(brown, yellow):
-    ab = brown + yellow
-    for b in range(1, ab + 1):  # a,b를 구해야 함
-        if (ab % b) == 0:  # ab의 약수
-            a = int(ab / b)
-            if a >= b:
-                if (a - 2) * (b - 2) == yellow:
-                    return [a, b]
+    num = brown + yellow
+    answer = []
+    
+    for h in range(3,num+1):
+        w = num//h
+        if (w - 2) * (h - 2) == yellow:
+            return [w, h]
