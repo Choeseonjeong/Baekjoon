1a79788e73a34dce8c1d60ef3864c136c8e2bdc2
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-06-23 12:21:42 +0900

    [level 2] Title: 카펫, Time: 0.14 ms, Memory: 9.07 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,8 +1,6 @@
 def solution(brown, yellow):
     num = brown + yellow
-    answer = []
-    
-    for h in range(3,num+1):
+    for h in range(1,num+1):
         w = num//h
-        if (w - 2) * (h - 2) == yellow:
-            return [w, h]
+        if (w-2)*(h-2)==yellow:
+            return [w,h]
