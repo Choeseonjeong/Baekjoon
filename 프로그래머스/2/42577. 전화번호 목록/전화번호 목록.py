93870dde35bf3ce96e2f2169799400c141e89875
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-07-29 10:23:11 +0900

    [level 2] Title: 전화번호 목록, Time: 505.78 ms, Memory: 45.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,12 @@
+def solution(phone_book):
+    dic = {}
+    for num in phone_book:
+        dic[num]=1
+        
+    for nums in phone_book:
+        arr = ''
+        for num in nums:
+            arr+=num
+            if arr in dic and arr != nums:
+                return False
+    return True
