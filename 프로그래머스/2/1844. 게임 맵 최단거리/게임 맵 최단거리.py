2a79814118c5e10a2c9a47911391862692bef6ac
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-08-12 20:09:35 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 21.91 ms, Memory: 9.33 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,36 +1,35 @@
 from collections import deque
+
 def solution(maps):
-    n,m = len(maps),len(maps[0]) # y,x
-    
+    answer = 0
     dx = [-1,1,0,0]
     dy = [0,0,-1,1]
-
+    
     def bfs(x,y):
         queue = deque()
         queue.append((x,y))
-        while queue: # 길이 끝나면 nx,ny는 큐에서 사라짐 
-            # for이 한 블럭 
+        
+        while queue:
             x,y = queue.popleft()
+            
             for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                if nx < 0 or nx >= n or ny < 0 or ny >= m:
-                    continue
-                # 벽이면
-                if maps[nx][ny]==0:
-                    continue
-                # 길이면
-                if maps[nx][ny]==1:
-                    maps[nx][ny] = maps[x][y] + 1 
+                nx = dx[i] + x
+                ny = dy[i] + y
+                # 벽 밖
+                if nx < 0 or nx >= len(maps) or ny < 0 or ny >= len(maps[0]): continue
+                # 벽일 때
+                if maps[nx][ny] == 0: continue
+                # 맞을 때
+                if maps[nx][ny] == 1: 
+                    maps[nx][ny] = maps[x][y] + 1
                     queue.append((nx,ny))
-        if maps[n-1][m-1] == 1:
-            return -1
-        else:
-            return maps[n-1][m-1]
-    return bfs(0, 0)
-            
-            
-            
-            
-            
-    
+        return maps[len(maps)-1][len(maps[0])-1]
+    answer = bfs(0, 0)
+    return -1 if answer == 1 else answer 
+                
+                
+                
+                
+                
+                
+                
