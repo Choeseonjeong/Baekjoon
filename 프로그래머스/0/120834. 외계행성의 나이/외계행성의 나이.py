465d7a3dc733463ace8344acd8253bff00ddc0d2
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-08-13 21:19:03 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.02 ms, Memory: 9.45 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.py
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.py
@@ -1,7 +1,5 @@
 def solution(age):
     answer = ''
-    proAge = {chr(i): i - ord('a') for i in range(ord('a'), ord('j') + 1)}
-    reversedProAge = {v: k for k, v in proAge.items()}
-    for i in list(str(age)):
-        answer+=str(reversedProAge[int(i)])
+    for i in str(age):
+        answer += chr(int(i) + 97)
     return answer
