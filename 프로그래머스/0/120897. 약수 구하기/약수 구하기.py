e9ea5ed4e11ae494da54a086b6ff553a41ab3266
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-08-13 21:04:05 +0900

    [level 0] Title: 약수 구하기, Time: 0.53 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
@@ -1,6 +1,6 @@
 def solution(n):
-    result = []
-    for num in range(1,n+1):
-        if n%num==0:
-            result.append(num)
-    return result
+    arr = []
+    for i in range(1,n+1):
+        if n%i==0:
+            arr.append(i)
+    return arr
