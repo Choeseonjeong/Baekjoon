d3f27bff33d09ea478ac02a133a0ff042b4a5b02
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-08-13 19:35:55 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.01 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.py
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.py
@@ -1,4 +1,4 @@
 def solution(numbers):
-    for i in numbers:
-        k = sorted(numbers)
-        return int(k[-1]) * int(k[-2])
+    answer = 0
+    numbers.sort()
+    return numbers[-1]*numbers[-2]
