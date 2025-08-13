d34d3a393e644da8f5e17f08d2d91d68d52655e1
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-08-13 20:57:49 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.02 ms, Memory: 9.37 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -1,7 +1,7 @@
 def solution(my_string):
-    arr = ['1','2','3','4','5','6','7','8','9','0']
-    answer =[]
-    for i in list(my_string):
-        if i in arr:
+    answer = []
+    for i in my_string:
+        if i.isdigit():
             answer.append(int(i))
-    return sorted(answer)
+    answer.sort()
+    return answer
