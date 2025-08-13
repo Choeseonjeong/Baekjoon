5037e1404015cc60b804a4309d68a941d7510198
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-08-13 21:41:10 +0900

    [level 0] Title: 합성수 찾기, Time: 0.25 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -1,13 +1,17 @@
-
 def solution(n):
-    num = []
-    count = 0
-    for i in range(2,n+1):
-        for j in range(1,i+1):
-            if i % j == 0 :
-                num.append(i)
-        if num.count(i) >=3:
-            count += 1
-    return count
+    answer = 0
+    def multi(x):
+        count = 0
+        for i in range(1,x+1):
+            if x % i == 0:
+                count+=1
+        if count>=3:
+            return 1
+        else:
+            return 0
     
+    for x in range(1,n+1):
+        answer+= multi(x)
+    return answer
+        
     
