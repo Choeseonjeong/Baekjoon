ef22f5f084904e2fdeea3d26c14da72b2c746854
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-08-17 00:28:56 +0900

    [level 0] Title: 수 조작하기 1, Time: 7.85 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -1,8 +1,6 @@
 def solution(n, control):
-    arr = { "w":1, "s":-1, "d":10, "a":-10 }
     answer = n
-    for i in list(control):
-        if i in arr.keys():
-            answer += arr[i]
+    arr = {"w":1,"s":-1,"d":10,"a":-10}
+    for i in control:
+        answer+=arr[i]
     return answer
-            
