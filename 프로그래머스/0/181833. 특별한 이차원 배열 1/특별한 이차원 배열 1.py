c6dc3f24584219674f0d5b33c8fadc922722cb14
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-08-19 19:26:14 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.33 ms, Memory: 9.74 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.py
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.py
@@ -1,6 +1,5 @@
 def solution(n):
-    answer = [[0 for j in range(n)] for i in range(n)]
-    for num in range(n):
-        answer[num][num]+=1
+    answer = [[0 for _ in range(n)] for _ in range(n)]
+    for i in range(n):
+        answer[i][i]+=1
     return answer
-    
