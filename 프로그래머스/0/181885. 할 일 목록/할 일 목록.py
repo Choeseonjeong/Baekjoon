5df99e3a3127b0daee58251af4129261f8827b48
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-08-20 16:58:52 +0900

    [level 0] Title: 할 일 목록, Time: 0.02 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
@@ -1,7 +1,6 @@
 def solution(todo_list, finished):
-    arr = dict(zip(todo_list,finished))
     result = []
-    for key, values in arr.items():
-        if values==False:
+    for key, value in zip(todo_list, finished):
+        if value == False:
             result.append(key)
     return result
