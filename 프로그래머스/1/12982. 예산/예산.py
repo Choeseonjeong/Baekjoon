5dff3d420daf082faa106af2170b9b16af87c507
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-08-26 18:29:59 +0900

    [level 1] Title: 예산, Time: 0.09 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.py
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -1,12 +1,10 @@
 def solution(d, budget):
-    d.sort()  # 작은 금액부터 처리하기 위해 정렬
-    total = 0  # 사용한 예산 합
-    cnt = 0  # 지원 가능한 부서 수
-    
-    for cost in d:
-        if total + cost > budget:  # 예산 초과 시 중단
+    answer = []
+    d.sort()
+    count = 0
+    for i in range(len(d)):
+        answer.append(d[i])
+        if sum(answer)>budget:
             break
-        total += cost
-        cnt += 1  # 부서 개수 증가
-
-    return cnt
+        count+=1
+    return count
