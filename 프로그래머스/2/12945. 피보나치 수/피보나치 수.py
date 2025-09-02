09a16fdeb4dcdc918d19369dbc3ae4c7d3fff25d
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-02 22:50:33 +0900

    [level 2] Title: 피보나치 수, Time: 556.11 ms, Memory: 456 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,5 +1,7 @@
 def solution(n):
-    a,b = 0,1
-    for _ in range(n):
-        a,b = b,(a+b)%1234567
-    return a
+    fibo = [0, 1]
+    for i in range(2, n+1):
+        fibo.append(fibo[i-1] + fibo[i-2])
+            
+    answer = fibo[-1]%1234567
+    return answer
