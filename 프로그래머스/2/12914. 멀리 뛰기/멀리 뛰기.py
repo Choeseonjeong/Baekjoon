1eb15ac7627bcaf895c75ea039e18f8fdd60e8fb
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-06 18:25:39 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.23 ms, Memory: 9.08 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,9 +1,6 @@
 def solution(n):
     answer = 0
-    a, b = 0,1
-    
-    for i in range(n) :
-        a,b = b, a+b
-    
-    answer = b % 1234567
-    return answer
+    a,b = 0,1
+    for i in range(n):
+        a,b = b,a+b
+    return b%1234567
