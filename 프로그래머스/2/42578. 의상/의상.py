c0b3625743c4ce8bb1910d7236c6af1b5829cf16
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-17 21:43:39 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -2,10 +2,10 @@ def solution(clothes):
     closet = {}
     for cloth, kind in clothes:
         if kind in closet.keys():
-            closet[kind]+=[cloth]
+            closet[kind] += [cloth]
         else:
-            closet[kind]=[cloth]
+            closet[kind] = [cloth]
     answer = 1
-    for key, values in closet.items():
-        answer *= (len(values)+1)
-    return answer-1
+    for cloth,kind in closet.items():
+        answer *= (len(kind)+1)
+    return answer -1
