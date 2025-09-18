d2bb7665d38f2e9597a3a5fe7497617016109b87
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-18 15:55:49 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,12 +1,13 @@
 def solution(n, lost, reserve):
-    reserve_set = set(reserve)-set(lost)
-    lost_set = set(lost)-set(reserve)
+    lost_set = set(lost)
+    lost_res = set(reserve)
+    both = lost_set & lost_res
+    lost_set-=both
+    lost_res-=both
     
-    for r in reserve_set:
-        if r-1 in lost_set:
-            lost_set.remove(r-1)
-        elif r+1 in lost_set:
-            lost_set.remove(r+1)
-            
-    return n-len(lost_set)
-
+    for i in sorted(lost_res):
+        if i-1 in lost_set:
+            lost_set.remove(i-1)
+        elif i+1 in lost_set:
+            lost_set.remove(i+1)
+    return n - len(lost_set)     
