0edca591f3585d8d66eed2a6250447e379a095c3
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-18 13:11:31 +0900

    [level 2] Title: 피로도, Time: 46.89 ms, Memory: 14.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,18 +1,17 @@
-import itertools # 모든 조합
+from itertools import permutations
 
-# 최대 돌 수 있는 던전 수 / 모두 돌아야 함
-def solution(k, dun):
-    per_dun = list(itertools.permutations(dun))
-    total = []
-    
-    for dun in per_dun:
-        num = 0
-        k_copy = k 
-        for need_stress, stress in dun:
-            if k_copy < need_stress:
-                break
-            else:
-                k_copy -= stress
-                num += 1
-        total.append(num)
-    return max(total)
+def solution(k, dungeons):
+    all = []
+    count = []
+    for i in permutations(dungeons, len(dungeons)):
+        all.append(i)
+    for i in all:
+        power = k
+        cnt = 0
+        for j in i:
+            if power >= j[0]:
+                power -= j[1]
+                cnt +=1 
+        count.append(cnt)
+        
+    return max(count)
