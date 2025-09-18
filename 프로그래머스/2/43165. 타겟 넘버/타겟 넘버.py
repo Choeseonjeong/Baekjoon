2e00193d38d829b0c9e79f5819480147a18f99bf
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-18 21:21:36 +0900

    [level 2] Title: 타겟 넘버, Time: 171.13 ms, Memory: 48 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,11 +1,12 @@
 def solution(numbers, target):
+    count = 0
     leaves = [0]
     
-    for num in numbers: 
+    for num in numbers:
         temp = []
+        
         for leaf in leaves:
             temp.append(leaf-num)
             temp.append(leaf+num)
-        leaves = temp 
-
+        leaves = temp
     return leaves.count(target)
