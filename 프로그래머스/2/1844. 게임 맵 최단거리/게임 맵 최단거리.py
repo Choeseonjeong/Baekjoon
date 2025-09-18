34bc776a1cd3e6ad62f875d92f51bdb4bd678242
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-18 21:47:18 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 15.20 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -8,25 +8,30 @@ def solution(maps):
     def bfs(x,y):
         queue = deque()
         queue.append((x,y))
-
+        
         while queue:
             x,y = queue.popleft()
             for i in range(4):
                 nx = x + dx[i]
                 ny = y + dy[i]
-
-                # 맵 넘어갈 때 컨티뉴
+                
+                # scope setting
                 if nx < 0 or ny < 0 or nx >= len(maps) or ny >= len(maps[0]): continue
-                # 벽일 때도 컨티뉴
+                # wall exist
                 if maps[nx][ny] == 0: continue
-                # 실행 
+                # success
                 if maps[nx][ny] == 1:
                     maps[nx][ny] = maps[x][y] + 1
                     queue.append((nx,ny))
-        return maps[len(maps)-1][len(maps[0])-1] 
+        return maps[len(maps)-1][len(maps[0])-1]
     answer = bfs(0,0)
-    return answer if answer!=1 else -1
-    
-
-            
-    
+    return -1 if answer == 1 else answer
+                
+                
+                
+                
+                
+                
+                
+                
+                
