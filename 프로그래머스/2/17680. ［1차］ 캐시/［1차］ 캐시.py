c38ea841ea06eb7b0c68aecef7160635e07ea820
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-19 13:47:17 +0900

    [level 2] Title: [1차] 캐시, Time: 366.83 ms, Memory: 23.5 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -1,18 +1,22 @@
 def solution(cacheSize, cities):
-    page = []
-    hit = 0
-    if cacheSize > 0:
-        for city in cities:
-            city = city.lower()
-            if city in page:
-                hit += 1
-                page.remove(city)
-                page.append(city)
-                page = page[-cacheSize:]
-            else: # page에 없다면
-                hit += 5
-                page.append(city)
-                page = page[-cacheSize:]
-        return hit
-    else:  
-        return len(cities)*5
+    cities = [i.lower() for i in cities]
+    answer = {}
+    time = 0
+    if cacheSize == 0:
+        return 5 * len(cities)
+    
+    for city in cities:
+        # 모두 1 추가
+        for k in list(answer.keys()):
+            answer[k] += 1
+        # 이미 존재하는건 0으로 초기화
+        if city in answer:
+            answer[city] = 0
+            time += 1
+        else: # 없으면 5 추가, 넣기, 삭제하기
+            time += 5
+            if len(answer) == cacheSize:
+                mmax = max(answer,key=answer.get)
+                del answer[mmax]
+            answer[city] = 0
+    return time
