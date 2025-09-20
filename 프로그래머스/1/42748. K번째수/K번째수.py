e94e0565b9cb819672159a7c5a882fd1b83c7636
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-20 20:16:02 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,7 +1,7 @@
 def solution(array, commands):
     answer = []
-    for n in commands:
-        i,j,k = n
+    for com in commands:
+        i,j,k = com
         arr = sorted(array[i-1:j])
         answer.append(arr[k-1])
     return answer
