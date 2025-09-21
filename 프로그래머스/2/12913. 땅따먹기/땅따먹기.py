863d69b3cb9fe2abb3c4e0568162e115cb888f55
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-21 22:27:42 +0900

    [level 2] Title: 땅따먹기, Time: 208.40 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -1,6 +1,5 @@
 def solution(land):
-    n,m = len(land),len(land[0])
-    for i in range(1,n):
-        for j in range(m):
-            land[i][j] += max(land[i-1][k] for k in range(m) if k!=j)
+    for i in range(1, len(land)):
+        for j in range(len(land[i])):
+            land[i][j] += max(land[i-1][:j]+land[i-1][j+1:])
     return max(land[-1])
