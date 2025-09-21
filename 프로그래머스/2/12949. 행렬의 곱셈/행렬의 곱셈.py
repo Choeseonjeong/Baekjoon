84de82a1a1446b9eacaf316c757031e1e4725c7b
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-21 16:27:55 +0900

    [level 2] Title: 행렬의 곱셈, Time: 130.80 ms, Memory: 9.66 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,8 +1,10 @@
 def solution(arr1, arr2):
-    m, n, r = len(arr1), len(arr1[0]), len(arr2[0])
-    matrix = [[0 for _ in range(r)] for _ in range(m)]
-    for i in range(m):
-        for j in range(r):
-            for k in range(n):
-                matrix[i][j] += arr1[i][k] * arr2[k][j]
-    return matrix
+    ans = []
+    m,n,k = len(arr1),len(arr1[0]),len(arr2[0])
+    num = [[0] * k for _ in range(m)]
+    
+    for a in range(m):
+        for b in range(k):
+            for c in range(n):
+                num[a][b] += arr1[a][c] * arr2[c][b]
+    return num
