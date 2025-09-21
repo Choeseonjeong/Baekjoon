0c07180fa3cabaee9572f5847cc346632ac9b96d
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-21 13:30:25 +0900

    [level 2] Title: 예상 대진표, Time: 0.00 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -1,7 +1,7 @@
 def solution(n,a,b):
-    answer = 0
-    while a!=b:
+    count = 0
+    while a != b:
         a = (a+1)//2
         b = (b+1)//2
-        answer+=1
-    return answer
+        count += 1
+    return count
