944a83b4dfe13219f5be4434dba953e43a22e5e9
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-22 20:24:05 +0900

    [level 1] Title: 모의고사, Time: 4.35 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,13 +1,19 @@
 def solution(answers):
-    answer = [[1, 2, 3, 4, 5],
-              [2, 1, 2, 3, 2, 4, 2, 5],
-              [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
-    result = []
-    for people in answer:
-        count = 0
-        for i in range(len(answers)):
-            if answers[i] == people[i%len(people)]:
-                count+=1
-        result.append(count)
-    return [idx+1 for idx,j in enumerate(result) if j == max(result)]
+    people1 = [1, 2, 3, 4, 5]
+    people2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    people3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    score = [0,0,0]
+    answer = []
+    for i in range(len(answers)):
+        if answers[i] == people1[i%5] :
+            score[0] += 1
+        if answers[i] == people2[i%8] :
+            score[1] += 1
+        if answers[i] == people3[i%10] :
+            score[2] += 1
             
+    for idx, num in enumerate(score) :
+        if num == max(score) :
+            answer.append(idx +1)
+    
+    return answer
