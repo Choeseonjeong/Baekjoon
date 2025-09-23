3639cc9e955c2e8406df6f460961d88bf0522ed8
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-23 14:57:33 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -1,12 +1,11 @@
 def solution(skill, skill_trees):
-    arr = []
     count = 0
-    for s in skill_trees:
+    arr = []
+    for word in skill_trees:
         ch = ''
-        for i in s:
+        for i in word:
             if i in skill:
                 ch+=i
-        if skill[:len(ch)] == ch:     
-            count += 1
+        if skill[:len(ch)]==ch:
+            count+=1
     return count
-    
