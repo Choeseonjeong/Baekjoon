349c788977676ba08503455a7e4498f3382ef404
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-23 20:19:06 +0900

    [level 3] Title: 네트워크, Time: 0.50 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,17 +1,15 @@
 def solution(n, computers):
-    visited = [False]*n
-    answer = 0
     
     def dfs(n,computers,com,visited):
         visited[com] = True
         for connect in range(n):
-            if com!=connect and computers[com][connect]==1 and not visited[connect]:
+            if computers[com][connect] == 1 and not visited[connect]:
                 dfs(n,computers,connect,visited)
-    
+        
+    visited = [False for _ in range(len(computers))]
+    answer = 0
     for com in range(n):
-        if not visited[com]:
+        if visited[com] == False:
             dfs(n,computers,com,visited)
-            answer+=1
+            answer += 1
     return answer
-    
-    
