d2d21a5a7868978dd962e0b70e0dd7b0a6fd662e
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-23 17:03:42 +0900

    [level 3] Title: 단어 변환, Time: 1.07 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,24 +1,24 @@
 from collections import deque
 
 def solution(begin, target, words):
-    def differ(a,b):
-        count = 0
-        for i,j in zip(a,b):
-            if i!=j:
-                count+=1
-        if count == 1:
-            return True
-        return False
-    
-    queue = deque([(begin, 0)])
-    visited = [False]*len(words)
-    
-    while queue:
-        text, depth = queue.popleft()
-        if text == target:
-            return depth 
-        for idx, word in enumerate(words):
-            if not visited[idx] and differ(text,words[idx]):
-                visited[idx] = True
-                queue.append((words[idx], depth + 1))
+    if target not in words:
+        return 0
+
+    L = len(begin)
+    q = deque([(begin, 0)])
+    visited = set([begin])
+
+    while q:
+        word, depth = q.popleft()
+        if word == target:
+            return depth
+
+        for text in words:
+            if text in visited:
+                continue
+            diff = sum(1 for i in range(L) if word[i] != text[i])
+            if diff == 1:
+                visited.add(text)
+                q.append((text, depth + 1))
+
     return 0
