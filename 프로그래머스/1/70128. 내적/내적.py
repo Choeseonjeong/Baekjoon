df651b06240a3f661804e3d89749317559961e51
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-24 20:38:59 +0900

    [level 1] Title: 내적, Time: 0.13 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.py
+++ 프로그래머스/1/70128. 내적/내적.py
@@ -1,5 +1,5 @@
 def solution(a, b):
-    answer = 0
-    for i in range(len(a)):
-        answer+=a[i]*b[i]
-    return answer
+    num = 0
+    for i,j in zip(a,b):
+        num+=j*i
+    return num
