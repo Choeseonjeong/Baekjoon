c0c2de571b889bf4d40abfe208c348e30ac0be7d
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-25 00:30:18 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.py
+++ 프로그래머스/1/12901. 2016년/2016년.py
@@ -1,12 +1,9 @@
 def solution(a, b):
-    day = [31,29,31,30,31,30,31,31,30,31,30,31]
+    answer = [31,29,31,30,31,30,31,31,30,31,30,31]
     num = 0
-    result = 0
-    for i in range(0,a-1):
-        num += day[i]
+    for i in answer[:a-1]:
+        num+=i
     num+=b
-    while num >= 7: 
-        num=num-7
-        result+=1
-    date = ['FRI','SAT','SUN','MON','TUE','WED','THU']
-    return date[num-1]
+    day = ["THU","FRI","SAT","SUN","MON","TUE","WED"]
+    
+    return day[num%7]
