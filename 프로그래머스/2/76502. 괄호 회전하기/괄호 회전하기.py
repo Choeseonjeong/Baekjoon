9272c87f3b6f7d2866eef9362ac771ebe4768ec4
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-24 20:47:15 +0900

    [level 2] Title: 괄호 회전하기, Time: 283.56 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,18 +1,18 @@
+def bracket(s):
+        stack = []
+        for i in s:
+            if len(stack) == 0: stack.append(i)
+            else:
+                if i == ")" and stack[-1] == "(":   stack.pop()
+                elif i == "]" and stack[-1] == "[":   stack.pop()
+                elif i == "}" and stack[-1] == "{":   stack.pop()
+                else: stack.append(i)
+        return 1 if len(stack) == 0 else 0
+        
 def solution(s):
     answer = 0
-    s = list(s)
-    for _ in range(len(s)):
-        stack = []
-        for i in range(len(s)):
-            if len(stack) > 0:
-                if stack[-1] == '[' and s[i] == ']': stack.pop()
-                elif stack[-1] == '{' and s[i] == '}': stack.pop()
-                elif stack[-1] == '(' and s[i] == ')': stack.pop()
-                else:
-                    stack.append(s[i])
-            else:
-                stack.append(s[i])
-        if len(stack) == 0:
-            answer += 1
-        s.append(s.pop(0))
+    
+    for i in range(len(s)):
+        if bracket(s):  answer +=1
+        s = s[1:] + s[:1]
     return answer
