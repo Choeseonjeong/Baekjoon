dc466905ba78af07470f98eb1f69e3fa1e28311d
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-25 00:04:19 +0900

    [level 1] Title: 소수 찾기, Time: 3276.66 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,13 +1,15 @@
 def solution(n):
-    count = 0
-    def prime(n):
-        if n < 2:  
+    answer = 0
+    def isprime(num):
+        if num < 2:
             return False
-        for i in range(2, int(n**0.5) + 1):
-            if n%i == 0:
+        for i in range(2,int(num**0.5)+1):
+            if num%i==0:
                 return False
         return True
-    
-    for i in range(1,n+1):
-        count+=prime(i)
+    count = 0
+    for num in range(1,n+1):
+        if isprime(num):
+            count+=1
     return count
+            
