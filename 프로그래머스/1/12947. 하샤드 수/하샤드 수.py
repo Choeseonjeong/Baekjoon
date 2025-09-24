273c4bec4a2973351b958d703623044f3975364e
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-24 20:25:12 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.py
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.py
@@ -1,3 +1,8 @@
 def solution(x):
-    answer = sum([int(i) for i in str(x)])
-    return True if x%answer == 0 else False
+    num = 0
+    for i in str(x):
+        num+=int(i)
+    if x%num==0:
+        return True
+    else:
+        return False
