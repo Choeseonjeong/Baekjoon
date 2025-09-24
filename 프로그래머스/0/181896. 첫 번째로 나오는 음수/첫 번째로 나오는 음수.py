45cdf7ea425fa0eb4de2fcf2e1d3e875e465c38e
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-25 00:46:12 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.01 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
@@ -1,5 +1,6 @@
 def solution(num_list):
-    for i, num in enumerate(num_list):
-        if num < 0:
-            return i
+    answer = 0
+    for idx, num in enumerate(num_list):
+        if num<0:
+            return idx
     return -1
