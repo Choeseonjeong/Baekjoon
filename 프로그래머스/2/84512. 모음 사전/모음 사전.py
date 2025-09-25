adb0477197b58db974f5dc98e627eb0413c9fa94
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-25 21:40:13 +0900

    [level 2] Title: 모음 사전, Time: 3.38 ms, Memory: 9.49 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,10 +1,10 @@
 from itertools import product
 
 def solution(word):
-    words = ['A', 'E', 'I', 'O', 'U']
-    all = []
-    for i in range(1,6):
-        for j in product(words,repeat=i):
-            all.append(''.join(j))
-    all.sort()
-    return all.index(word)+1
+    text = ['A', 'E', 'I', 'O', 'U']
+    all = set()
+    for i in range(1,len(text)+1):
+        for j in product(text,repeat=i):
+            ch =''.join(j)
+            all.add(ch)
+    return sorted(list(all)).index(word)+1
