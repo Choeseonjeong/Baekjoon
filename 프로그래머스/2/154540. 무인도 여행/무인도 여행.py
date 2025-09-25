6c54ac348baaf69f72bebe79d6e2fadeb8770611
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-25 20:49:39 +0900

    [level 2] Title: 무인도 여행, Time: 23.29 ms, Memory: 9.51 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
@@ -1,26 +1,31 @@
-import sys
-sys.setrecursionlimit(10**6)
+from collections import deque
 
 def solution(maps):
-    n,m = len(maps),len(maps[0])
+    n, m = len(maps), len(maps[0])
     visited = [[False]*m for _ in range(n)]
     answer = []
-    def dfs(i,j,visited,maps,n,m):
-        dirs = [(-1,0),(1,0),(0,-1),(0,1)]
-        visited[i][j] = True
-        num = int(maps[i][j])
-        
-        for dx, dy in dirs:
-            x, y = i + dx, j + dy
-            if (0<=x<n) and (0<=y<m) and not visited[x][y] and maps[x][y] != "X":
-                num += dfs(x, y, visited, maps, n,m)
-        return num
     
-    for i in range(n):
-        for j in range(m):
-            if maps[i][j] != "X" and not visited[i][j]:
-                answer.append(dfs(i,j,visited,maps,n,m))
-    if answer:
-        return sorted(answer)
-    else:
-        return [-1]
+    def bfs(sx, sy):
+        queue = deque()
+        queue.append((sx, sy))
+        visited[sy][sx] = True
+        total = int(maps[sy][sx])
+        
+        dx, dy = [0,0,-1,1],[1,-1,0,0]
+        
+        while queue:
+            x, y = queue.popleft()
+            for i in range(4):
+                nx, ny = x+dx[i], y+dy[i]
+                if 0<=nx<m and 0<=ny<n and maps[ny][nx]!="X" and not visited[ny][nx]:
+                    visited[ny][nx] = True
+                    total += int(maps[ny][nx])
+                    queue.append((nx, ny))
+        return total
+    
+    for y in range(n):
+        for x in range(m):
+            if maps[y][x] != "X" and not visited[y][x]:
+                answer.append(bfs(x,y))
+    
+    return sorted(answer) if answer else [-1]
