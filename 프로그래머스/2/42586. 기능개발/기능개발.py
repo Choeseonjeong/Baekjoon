14251080a94a69d2009321ecd4c53aa9adba6480
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-25 14:27:44 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,15 +1,17 @@
 import math
+
 def solution(progresses, speeds):
-    arr = [math.ceil((100-p)/s) for p,s in zip(progresses, speeds)]
-    result = []
+    answer = []
+    days = [math.ceil((100-p)/s) for p,s in zip(progresses, speeds)]
+    
+    cur = days[0]
     count = 1
-    bepo = arr[0]
-    for i in range(1,len(arr)):
-        if bepo >= arr[i]:
-            count+=1
+    for i in range(1,len(days)):
+        if cur >= days[i]:
+            count += 1
         else:
-            result.append(count)
+            answer.append(count)
             count = 1
-            bepo = arr[i]
-    result.append(count)
-    return result
+            cur = days[i]
+    answer.append(count)
+    return answer
