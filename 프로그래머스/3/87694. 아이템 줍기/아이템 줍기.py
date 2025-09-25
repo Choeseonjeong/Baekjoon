71ec0620628e889320d5f6226c3f9e9c2281e8f2
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-25 22:41:10 +0900

    [level 3] Title: 아이템 줍기, Time: 2.20 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
@@ -1,39 +1,39 @@
 from collections import deque
-
-def solution(rectangle, characterX, characterY, itemX, itemY):
-    field = [[-1] * 102 for i in range(102)]
-    
-    for r in rectangle:
-        x1, y1, x2, y2 = map(lambda x:x*2, r)
-        for i in range(x1,x2+1):
-            for j in range(y1,y2+1):
-                if (x1 < i < x2) and (y1 < j < y2):
-                    field[i][j] = 0
-                elif field[i][j] != 0:
-                    field[i][j] = 1
-    
-    dx = [-1,1,0,0]
-    dy = [0,0,-1,1]
-    
+def solution(rectangle, characterX, characterY, itemX, itemY): 
+    x1,y1,x2,y2 = 0,0,0,0
+    n,m = len(rectangle),len(rectangle[0])
+    maxSize = 102
+    maps = [[0]*maxSize for _ in range(maxSize)]
+    dist = [[-1]*maxSize for _ in range(maxSize)]
+    for x1,y1,x2,y2 in rectangle:
+        x1,y1,x2,y2 = x1*2,y1*2,x2*2,y2*2
+        for x in range(x1,x2+1):
+            for y in range(y1,y2+1):
+                maps[y][x] = 1
+    for x1,y1,x2,y2 in rectangle:
+        x1,y1,x2,y2 = x1*2,y1*2,x2*2,y2*2
+        for x in range(x1+1,x2):
+            for y in range(y1+1,y2):
+                maps[y][x] = 0
+            
+    characterX, characterY, itemX, itemY = characterX*2, characterY*2, itemX*2, itemY*2
+    dist[characterY][characterX] = 0
     queue = deque()
-    queue.append((characterX*2, characterY*2))
-    visited = [[1]* 102 for _ in range(102)]
-    visited[characterX * 2][characterY * 2] = 0
-    answer = 0
+    queue.append((characterX,characterY))
+    dx,dy = [-1,1,0,0],[0,0,-1,1]
     
     while queue:
         x,y = queue.popleft()
-        if (x == itemX*2) and (y == itemY*2):
-            answer = visited[x][y]//2
-            break
-            
+        if (x==itemX) and (y==itemY):
+            return dist[y][x]//2
         for i in range(4):
-            nx,ny = x+dx[i],y+dy[i]
-            
-            if field[nx][ny] == 1 and visited[nx][ny] == 1:
-                queue.append([nx, ny])
-                visited[nx][ny] = visited[x][y] + 1
-                
-    return answer
-        
-        
+            nx = x+dx[i]
+            ny = y+dy[i]
+            if (0<=nx<maxSize) and (0<=ny<maxSize) and maps[ny][nx] == 1 and dist[ny][nx] ==-1:
+                dist[ny][nx] = dist[y][x] + 1
+                queue.append((nx,ny))
+    return -1
+    
+    
+    
+    
