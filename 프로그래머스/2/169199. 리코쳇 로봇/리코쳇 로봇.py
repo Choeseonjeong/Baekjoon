2d1c92ad7bd8c69eb2f10ebe298a88274159060e
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-25 20:26:22 +0900

    [level 2] Title: 리코쳇 로봇, Time: 9.80 ms, Memory: 9.37 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
@@ -1,42 +1,43 @@
 from collections import deque
-
 def solution(board):
     n,m = len(board),len(board[0])
-    visited = [[987654321]* m for _ in range(n)]
-    sx,sy = 0,0
-    
-    for i in range(n):
-        for j in range(m):
-            if board[i][j] == "R":
-                sx,sy = i,j
-    visited[sx][sy] = 0
-    
+    sx,sy,ex,ey = 0,0,0,0
+    for y in range(n):
+        for x in range(m):
+            if board[y][x] == "R":
+                sx,sy = x,y
+            elif board[y][x] == "G":
+                ex,ey = x,y
+    visited = [[987654321] * m for _ in range(n)]
+
     queue = deque()
     queue.append((sx,sy,0))
-    
     dx,dy = [-1,1,0,0],[0,0,-1,1]
+    visited[sy][sx] = 0
+    
     while queue:
         x,y,depth = queue.popleft()
-        
-        if board[x][y] == "G":
+        if (x==ex) and (y==ey):
             return depth
-        
         for i in range(4):
             nx,ny = x,y
-            while (0<=nx+dx[i]<n) and (0<=ny+dy[i]<m) and board[nx+dx[i]][ny+dy[i]] != "D":
-                nx += dx[i]
-                ny += dy[i]
-                
-            if visited[nx][ny] > depth + 1:
-                visited[nx][ny] = depth + 1
+            while (0<=nx+dx[i]<m) and (0<=ny+dy[i]<n) and board[ny+dy[i]][nx+dx[i]] != "D":
+                nx+=dx[i]
+                ny+=dy[i]
+            if visited[ny][nx] > visited[y][x] + 1:
+                visited[ny][nx] = visited[y][x] + 1
                 queue.append((nx,ny,depth+1))
     return -1
             
-                
-                
-                
-                
-                
-                
-                
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
         
