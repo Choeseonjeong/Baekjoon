dbe5207d67602c59a6f62fd1a86426d0ad19f6db
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-25 14:11:34 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 53.60 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,15 +1,15 @@
 def solution(participant, completion):
-    people = {}
+    peoples = dict()
     for i in participant:
-        if i in people:
-            people[i] += 1
+        if i not in peoples:
+            peoples[i] = 1
         else:
-            people[i] = 1
+            peoples[i] += 1
     for i in completion:
-        if i in people:
-            people[i]-=1
-    result = ''
-    for key,value in people.items():
-        if value != 0:
-            result+=key
-    return result
+        peoples[i] -= 1 
+        if peoples[i]==0:
+            peoples.pop(i)
+    answer = ''
+    for i in peoples.keys():
+        answer+=i
+    return answer
