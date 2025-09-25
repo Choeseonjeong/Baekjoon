030ad39c06e2e1ed0891ee6f2fe14d9f0b2a7ffc
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-25 16:17:26 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 9.09 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,5 +1,5 @@
-def solution(brown, yellow):  
-    x = (brown + 4 + ((brown + 4) ** 2 - 16 * (brown + yellow)) ** 0.5) / 4
-    y = (brown + yellow) / x
-
-    return [x, y]
+import math
+def solution(brown, yellow):
+    w = ((brown+4)/2 + math.sqrt(((brown+4)/2)**2-4*(brown+yellow)))/2
+    h = ((brown+4)/2 - math.sqrt(((brown+4)/2)**2-4*(brown+yellow)))/2
+    return [w,h]
