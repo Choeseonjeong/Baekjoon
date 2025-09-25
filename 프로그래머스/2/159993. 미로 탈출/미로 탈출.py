9f0e3fbfa9d8aa74e2218a2ef1a1783dc05d930e
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-25 20:06:02 +0900

    [level 2] Title: 미로 탈출, Time: 15.38 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
@@ -1,41 +1,40 @@
 from collections import deque
-
 def solution(maps):
-    n, m = len(maps), len(maps[0])
-
-    def bfs(start, end):
-        sx, sy = start
-        ex, ey = end
-        dist = [[-1]*m for _ in range(n)]
-        q = deque([(sx, sy)])
-        dist[sx][sy] = 0
-
-        dx = (-1, 1, 0, 0)
-        dy = (0, 0, -1, 1)
-
-        while q:
-            x, y = q.popleft()
-            if (x, y) == (ex, ey):
-                return dist[x][y]
-            for k in range(4):
-                nx, ny = x + dx[k], y + dy[k]
-                if 0 <= nx < n and 0 <= ny < m:
-                    if maps[nx][ny] != 'X' and dist[nx][ny] == -1:
-                        dist[nx][ny] = dist[x][y] + 1
-                        q.append((nx, ny))
+    
+    def findXY(start,end):
+        sx,sy,ex,ey = 0,0,0,0
+        for y in range(n):
+            for x in range(m):
+                if maps[y][x] == start:
+                    sx, sy = x, y  
+                elif maps[y][x] == end:
+                    ex, ey = x, y
+        return sx,sy,ex,ey
+    
+    def bfs(start,end):
+        dist = [[-1] * m for _ in range(n)]
+        sx,sy,ex,ey= findXY(start,end)
+        dx,dy = [-1,1,0,0],[0,0,-1,1]
+        
+        queue = deque()
+        queue.append((sx,sy))
+        dist[sy][sx] = 0
+        
+        while queue:
+            x,y = queue.popleft()
+            if (x==ex) and (y==ey):
+                return dist[y][x]
+            
+            for i in range(4):
+                nx,ny = x+dx[i],y+dy[i]
+                if (0<=nx<m) and (0<=ny<n) and maps[ny][nx]!= "X" and dist[ny][nx] == -1:
+                    dist[ny][nx] = dist[y][x]+1
+                    queue.append((nx,ny))
         return -1
-
-    S = L = E = None
-    for i in range(n):
-        for j in range(m):
-            if maps[i][j] == 'S': S = (i, j)
-            elif maps[i][j] == 'L': L = (i, j)
-            elif maps[i][j] == 'E': E = (i, j)
-
-    to_lever = bfs(S, L)
-    if to_lever == -1:
+    
+    n,m = len(maps),len(maps[0])
+    first = bfs("S", "L")
+    second = bfs("L", "E")
+    if first == -1 or second == -1:
         return -1
-    to_exit = bfs(L, E)
-    if to_exit == -1:
-        return -1
-    return to_lever + to_exit
+    return first + second
