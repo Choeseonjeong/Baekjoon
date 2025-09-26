34f39463e38ce956cb51fa075059538724f10f8c
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-26 16:42:44 +0900

    [level 3] Title: 아이템 줍기, Time: 2.03 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
@@ -1,39 +1,37 @@
 from collections import deque
-def solution(rectangle, characterX, characterY, itemX, itemY): 
-    x1,y1,x2,y2 = 0,0,0,0
-    n,m = len(rectangle),len(rectangle[0])
-    maxSize = 102
-    maps = [[0]*maxSize for _ in range(maxSize)]
-    dist = [[-1]*maxSize for _ in range(maxSize)]
-    for x1,y1,x2,y2 in rectangle:
-        x1,y1,x2,y2 = x1*2,y1*2,x2*2,y2*2
-        for x in range(x1,x2+1):
-            for y in range(y1,y2+1):
+def solution(rectangle, characterX, characterY, itemX, itemY):
+    MAX = 102
+    maps = [[0]*MAX for _ in range(MAX)]
+    dist = [[-1]*MAX for _ in range(MAX)]
+    for sx, sy, ex, ey in rectangle:
+        sx, sy, ex, ey = sx*2, sy*2, ex*2, ey*2
+        for y in range(sy, ey+1):
+            for x in range(sx, ex+1):
                 maps[y][x] = 1
-    for x1,y1,x2,y2 in rectangle:
-        x1,y1,x2,y2 = x1*2,y1*2,x2*2,y2*2
-        for x in range(x1+1,x2):
-            for y in range(y1+1,y2):
+
+    for sx, sy, ex, ey in rectangle:
+        sx, sy, ex, ey = sx*2, sy*2, ex*2, ey*2
+        for y in range(sy+1, ey):
+            for x in range(sx+1, ex):
                 maps[y][x] = 0
-            
+
     characterX, characterY, itemX, itemY = characterX*2, characterY*2, itemX*2, itemY*2
-    dist[characterY][characterX] = 0
+    # maps = 길인지 확인, dist = 숫자 더하기 
     queue = deque()
-    queue.append((characterX,characterY))
+    queue.append((characterX, characterY))
+    dist[characterY][characterX] = 0
     dx,dy = [-1,1,0,0],[0,0,-1,1]
     
     while queue:
         x,y = queue.popleft()
         if (x==itemX) and (y==itemY):
-            return dist[y][x]//2
+            return dist[itemY][itemX]//2
+        
         for i in range(4):
             nx = x+dx[i]
             ny = y+dy[i]
-            if (0<=nx<maxSize) and (0<=ny<maxSize) and maps[ny][nx] == 1 and dist[ny][nx] ==-1:
+            if (0<=nx<MAX) and (0<=ny<MAX) and maps[ny][nx]==1 and dist[ny][nx] == -1:
                 dist[ny][nx] = dist[y][x] + 1
                 queue.append((nx,ny))
     return -1
-    
-    
-    
-    
+            
