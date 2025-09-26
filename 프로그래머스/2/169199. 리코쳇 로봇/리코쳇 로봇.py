208b078a36fd35c28bca0bcbfc554407472052c4
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-26 15:43:13 +0900

    [level 2] Title: 리코쳇 로봇, Time: 10.77 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
@@ -1,43 +1,37 @@
 from collections import deque
 def solution(board):
     n,m = len(board),len(board[0])
-    sx,sy,ex,ey = 0,0,0,0
-    for y in range(n):
-        for x in range(m):
-            if board[y][x] == "R":
-                sx,sy = x,y
-            elif board[y][x] == "G":
-                ex,ey = x,y
-    visited = [[987654321] * m for _ in range(n)]
-
-    queue = deque()
-    queue.append((sx,sy,0))
+    
+    def findXY(word):
+        for y in range(n):
+            for x in range(m):
+                if board[y][x]==word:
+                    return x,y
+    
+    sx,sy= findXY("R")
+    ex,ey = findXY("G")
+    
+    dist = [[987654321]*m for _ in range(n)]
+    dist[sy][sx] = 0
+    
     dx,dy = [-1,1,0,0],[0,0,-1,1]
-    visited[sy][sx] = 0
+    
+    queue = deque()
+    queue.append((sx,sy))
     
     while queue:
-        x,y,depth = queue.popleft()
+        x,y = queue.popleft()
         if (x==ex) and (y==ey):
-            return depth
+            return dist[y][x]
+        
         for i in range(4):
             nx,ny = x,y
             while (0<=nx+dx[i]<m) and (0<=ny+dy[i]<n) and board[ny+dy[i]][nx+dx[i]] != "D":
                 nx+=dx[i]
                 ny+=dy[i]
-            if visited[ny][nx] > visited[y][x] + 1:
-                visited[ny][nx] = visited[y][x] + 1
-                queue.append((nx,ny,depth+1))
+            if dist[ny][nx] > dist[y][x] + 1:
+                dist[ny][nx] = dist[y][x] + 1
+                queue.append((nx,ny))
     return -1
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-        
+    
+    
