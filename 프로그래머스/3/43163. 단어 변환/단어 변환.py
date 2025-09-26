ad39c3c59c1883f47587f6a9ce6c7170da5d6069
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-26 16:23:35 +0900

    [level 3] Title: 단어 변환, Time: 7.07 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,5 +1,4 @@
 from collections import deque
-
 def solution(begin, target, words):
     def differ(a,b):
         count = 0
@@ -8,19 +7,18 @@ def solution(begin, target, words):
                 count+=1
         return True if count==1 else False
     
-    def bfs(begin,target,words):
+    def bfs(begin,depth):
         queue = deque()
-        queue.append([begin,0])
+        queue.append([begin,depth])
         
         while queue:
-            text, depth = queue.popleft()
-            if target==text:
+            text,depth = queue.popleft()
+            if text == target:
                 return depth
-        
-            for num in words:
-                if differ(text,num):
-                    queue.append((num,depth+1))
-                    
+            for i in range(len(words)):
+                if differ(text,words[i]):
+                    queue.append((words[i],depth+1))
+    
     if target not in words:
         return 0
-    return bfs(begin,target,words)
+    return bfs(begin,0)
