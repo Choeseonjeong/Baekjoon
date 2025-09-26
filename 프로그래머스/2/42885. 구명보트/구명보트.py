dab43719d6bcf934f76d935a6e59192143db3d02
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-26 12:27:06 +0900

    [level 2] Title: 구명보트, Time: 8.98 ms, Memory: 9.55 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,13 +1,13 @@
 def solution(people, limit):
-    ans = 0
-    front = 0
-    back = len(people)-1
     people.sort()
-    
-    while front < back:
-        if people[front]+people[back]<=limit:
-            ans += 1
-            front += 1
-        back -= 1
-    return len(people)-ans
-        
+    front, back = 0,len(people)-1
+    count = 0
+    while front <= back:
+        if people[front] + people[back] > limit:
+            back -= 1
+            count += 1
+        else:
+            front+=1
+            back-=1
+            count+=1
+    return count
