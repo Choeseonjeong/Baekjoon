4171cb753d2d16c5e66cf3ce52a2a9603a3b41a1
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-26 15:33:12 +0900

    [level 2] Title: 미로 탈출, Time: 16.33 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
@@ -1,24 +1,24 @@
 from collections import deque
 def solution(maps):
-    
+    n,m = len(maps),len(maps[0])  
     def findXY(start,end):
         sx,sy,ex,ey = 0,0,0,0
         for y in range(n):
             for x in range(m):
                 if maps[y][x] == start:
-                    sx, sy = x, y  
+                    sx,sy = x,y
                 elif maps[y][x] == end:
-                    ex, ey = x, y
+                    ex,ey = x,y
         return sx,sy,ex,ey
-    
+
     def bfs(start,end):
-        dist = [[-1] * m for _ in range(n)]
-        sx,sy,ex,ey= findXY(start,end)
-        dx,dy = [-1,1,0,0],[0,0,-1,1]
+        sx,sy,ex,ey = findXY(start,end)
+        dist = [[-1]*m for _ in range(n)]
         
         queue = deque()
         queue.append((sx,sy))
         dist[sy][sx] = 0
+        dx,dy = [-1,1,0,0],[0,0,-1,1]
         
         while queue:
             x,y = queue.popleft()
@@ -27,14 +27,13 @@ def solution(maps):
             
             for i in range(4):
                 nx,ny = x+dx[i],y+dy[i]
-                if (0<=nx<m) and (0<=ny<n) and maps[ny][nx]!= "X" and dist[ny][nx] == -1:
-                    dist[ny][nx] = dist[y][x]+1
+                if (0<=nx<m) and (0<=ny<n) and dist[ny][nx]==-1 and maps[ny][nx]!="X":
+                    dist[ny][nx] = dist[y][x] + 1
                     queue.append((nx,ny))
         return -1
     
-    n,m = len(maps),len(maps[0])
-    first = bfs("S", "L")
-    second = bfs("L", "E")
+    first = bfs("S","L")
+    second = bfs("L","E")
     if first == -1 or second == -1:
         return -1
-    return first + second
+    return first+second
