281ee95f1b5528f2a159ec1bcb5ec290d32c6832
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-26 12:53:18 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.13 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,9 +1,11 @@
 def solution(n, words):
-    stack = [words[0]]
-    result = [0,0]
+    
+    checked = [words[0]]
+    
     for i in range(1,len(words)):
-        if stack[-1][-1] != words[i][0] or words[i] in stack:
-            return [i%n+1,i//n+1]
+        if words[i][0] == words[i-1][-1] and words[i] not in checked:
+            checked.append(words[i])
         else:
-            stack.append(words[i])
-    return result
+            return [(i%n)+1, (i//n)+1]
+
+    return [0,0]
