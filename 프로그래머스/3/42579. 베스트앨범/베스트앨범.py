c19d0c1f21a9774a7fd1948c1628fd19f32ffd94
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-26 16:13:09 +0900

    [level 3] Title: 베스트앨범, Time: 0.11 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,18 +1,20 @@
 def solution(genres, plays):
-    dic1 = {}
-    dic2 = {}
+    dic1 = dict()
+    dic2 = dict()
     answer = []
-    for idx,(g,p) in enumerate(zip(genres, plays)):
-        if g in dic1:
-            dic1[g] += [[idx,p]]
-        else:
-            dic1[g] = [[idx,p]]
-        if g in dic2:
-            dic2[g] +=p
-        else:
-            dic2[g] = p
     
-    for (k, v) in sorted(dic2.items(), key=lambda x:x[1], reverse=True):
-        for (i, p) in sorted(dic1[k], key=lambda x:x[1], reverse=True)[:2]:
-            answer.append(i)
+    for g,p in zip(genres, plays):
+        if g in dic1:
+            dic1[g] += p
+        else:
+            dic1[g] = p
+    for idx,(g,p) in enumerate(zip(genres, plays)):
+        if g in dic2:
+            dic2[g] += [[p,idx]]
+        else:
+            dic2[g] = [[p,idx]]
+            
+    for (k,v) in sorted(dic1.items(), key=lambda x: x[1], reverse=True):
+        for (i,j) in sorted(dic2[k],reverse=True,key=lambda x: x[0])[:2]:
+            answer.append(j)
     return answer
