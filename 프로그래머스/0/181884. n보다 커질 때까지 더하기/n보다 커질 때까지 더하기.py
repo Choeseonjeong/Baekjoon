eaf976b75439107efa11bcf0f7247f88f81a230b
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-29 19:08:31 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.01 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.py
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.py
@@ -1,7 +1,6 @@
 def solution(numbers, n):
-    answer = 0
-    idx = 0
-    while n>=answer:
+    answer,idx =0,0
+    while answer <= n:
         answer+=numbers[idx]
         idx+=1
     return answer
