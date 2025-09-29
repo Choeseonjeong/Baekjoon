3749cf7655537d99fb8fb42980fce5273101da6b
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-29 19:17:11 +0900

    [level 0] Title: l로 만들기, Time: 17.42 ms, Memory: 9.49 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.py
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.py
@@ -1,3 +1,9 @@
 def solution(myString):
-    answer = ''
-    return myString.replace("a","l").replace("b","l").replace("c","l").replace("d","l").replace("e","l").replace("f","l").replace("g","l").replace("h","l").replace("i","l").replace("j","l").replace("k","l")
+    answer = 'abcdefghijk'
+    ch = ''
+    for i in myString:
+        if i in answer:
+            ch+="l"
+        else:
+            ch+=i
+    return ch
