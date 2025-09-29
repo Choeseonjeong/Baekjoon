e9d74136431ba0df4e8008decafaf834001815d3
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-29 19:07:08 +0900

    [level 0] Title: 접미사 배열, Time: 0.05 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.py
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.py
@@ -1,5 +1,3 @@
 def solution(my_string):
-    answer = []
-    for i in range(len(my_string)):
-        answer.append(my_string[i:])
+    answer = [my_string[i:] for i in range(len(my_string))]
     return sorted(answer)
