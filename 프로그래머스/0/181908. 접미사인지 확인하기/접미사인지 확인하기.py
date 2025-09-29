79243b9ab68b9019af8fcf264303cfcf979d0237
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-29 20:19:44 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.02 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
@@ -1,6 +1,7 @@
 def solution(my_string, is_suffix):
-    answer = []
-    for i in range(len(my_string)):
-        if is_suffix == my_string[i:len(my_string)]:
+    answer = 0
+    ch = ''
+    for idx,ch in enumerate(my_string):
+        if is_suffix == my_string[idx:]:
             return 1
     return 0
