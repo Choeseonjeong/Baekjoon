7404fd54d1deaf9829329f48a88c3324cece5add
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-30 15:45:32 +0900

    [level 1] Title: 예산, Time: 0.06 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.py
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -1,10 +1,5 @@
 def solution(d, budget):
-    answer = []
     d.sort()
-    count = 0
-    for i in range(len(d)):
-        answer.append(d[i])
-        if sum(answer)>budget:
-            break
-        count+=1
-    return count
+    while budget < sum(d):
+        d.pop()
+    return len(d)
