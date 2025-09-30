c84e76d62cdc1553f648f015a1449dc97b45bcde
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-30 14:32:59 +0900

    [level 2] Title: 숫자의 표현, Time: 6.04 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,12 +1,14 @@
 def solution(n):
-    count = 0                      
-    for i in range(1, n+1):         
-        sumN = 0                     
-        for j in range(i, n+1):     
-            sumN += j               
-            if sumN == n:          
-                count += 1          
+    answer = 0
+    for i in range(1,n+1):
+        num = i
+        if i == n:
+            answer+=1
+        for j in range(i+1,n+1):
+            num+=j 
+            if num > n:
                 break
-            if sumN > n:          
+            elif num == n:
+                answer+=1
                 break
-    return count
+    return answer
