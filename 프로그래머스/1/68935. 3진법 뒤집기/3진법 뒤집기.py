c84a8b2e963d9b8dc6df398a314716d6761d0c53
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-09-30 15:48:22 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,6 +1,9 @@
 def solution(n):
     answer = ''
-    while n>0:
-        answer+=str(n%3)
-        n //=3
-    return int(answer,3)
+    
+    while(n >= 1):
+        rest = n % 3
+        n = n // 3
+        answer += str(rest)
+    
+    return int(answer, 3)
