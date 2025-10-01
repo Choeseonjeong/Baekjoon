fff0ddacdd7d43c918bafb6464ff6957105db9b8
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-10-01 20:41:30 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,5 +1,15 @@
 def solution(s):
-    arr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-    for i,c in enumerate(arr):
-        s = s.replace(c,str(i))
+    dic = {"zero":"0",
+          "one":"1",
+          "two":"2",
+          "three":"3",
+          "four":"4",
+          "five":"5",
+          "six":"6",
+          "seven":"7",
+          "eight":"8",
+          "nine":"9"}
+    for key,value in dic.items():
+        if key in s:
+            s = s.replace(key,value)
     return int(s)
