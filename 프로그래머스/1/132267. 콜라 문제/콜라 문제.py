2290253079c7d033a7f6876d5e26f5860b61aa91
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-10-01 22:00:43 +0900

    [level 1] Title: 콜라 문제, Time: 0.51 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -1,11 +1,7 @@
 def solution(a, b, n):
-    answer = 0
+    count = 0
     while n >= a:  
-        new_colas = (n // a) * b  
-        answer += new_colas  
-        n = new_colas + (n % a)
-    return answer   
-
-
-# a만큼의 빈병가져가면 b만큼의 콜라 줌
-# n개 가져다주면 몇 개 반환?
+        q, r = divmod(n, a) 
+        count += q * b      
+        n = q * b + r       
+    return count
