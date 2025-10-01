055850a1e0141f08bfb926859ce7f834ba6ba3bd
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-10-01 19:21:42 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 29.20 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -1,7 +1,10 @@
 def solution(s):
-    answerList = [-1]*len(s)
-    for i in range(len(s)):
-        for j in range(i):
-            if (s[i] == s[j]):
-                answerList[i] = i-j          
-    return answerList
+    answer = [-1 for _ in range(len(s))]
+    stack = []
+    for idx, text in enumerate(s):
+        for s in range(len(stack)-1,-1,-1):
+            if stack and stack[s] == text:
+                answer[idx] = idx - s
+                break
+        stack.append(text)
+    return answer
