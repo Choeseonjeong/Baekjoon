d31266e1a87363feec63c2f0077a523977f079e0
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-10-01 19:03:40 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.06 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,11 +1,11 @@
 def solution(s):
-    answer = ''
-    new_list = s.split(' ')
-    for i in new_list:
-        for j in range(len(i)):
-            if j % 2 == 0:
-                answer += i[j].upper()
+    result = []
+    for word in s.split(" "):
+        answer = ''
+        for idx in range(len(word)):
+            if idx%2==0:
+                answer += word[idx].upper()
             else:
-                answer += i[j].lower()
-        answer+= ' '
-    return answer[0:-1]
+                answer += word[idx].lower()
+        result.append(answer)
+    return ' '.join(result)
