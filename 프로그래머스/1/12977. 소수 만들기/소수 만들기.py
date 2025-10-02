53a06fa5cb3b7351f21ba318f39a741a2e4a0d9b
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-10-02 17:51:54 +0900

    [level 1] Title: 소수 만들기, Time: 22.57 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -1,19 +1,18 @@
 from itertools import combinations
-
 def solution(nums):
-    def isprime(num):
-        if num < 2:
+    answer = set()
+    count = 0
+    def isprime(n):
+        if n < 2:
             return False
-        for i in range(2,int(num**0.5)+1):
-            if num%i==0:
+        for i in range(2,int(n**0.5+1)):
+            if n%i==0:
                 return False
         return True
-    
-    count = 0
-    all = list((sum(i) for i in combinations(nums,3)))
-    
-    count = 0
-    for j in all:
-        if isprime(j):
-            count+=1
+
+    for i in combinations(nums,3):
+        answer.add(i)
+    for i in list(answer):
+        if isprime(sum(i)):
+            count += 1
     return count
