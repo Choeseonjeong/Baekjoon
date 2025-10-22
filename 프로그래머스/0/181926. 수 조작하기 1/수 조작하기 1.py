20cb770d118c9561d181ffe04483945f7ab40def
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-10-22 16:08:10 +0900

    [level 0] Title: 수 조작하기 1, Time: 7.48 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -1,6 +1,6 @@
 def solution(n, control):
     answer = n
-    arr = {"w":1,"s":-1,"d":10,"a":-10}
-    for i in control:
-        answer+=arr[i]
+    word = {"w":1,"s":-1,"d":10,"a":-10}
+    for text in control:
+        answer += word[text]
     return answer
