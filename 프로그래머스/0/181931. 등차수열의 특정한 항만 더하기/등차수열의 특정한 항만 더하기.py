4cdb44443949cf1cd35c94065015b2bfa509fcda
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-10-25 00:42:20 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.02 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
@@ -1,12 +1,6 @@
 def solution(a, d, included):
-    arr = []
-    sum = a
     answer = 0
-    for i in range(len(included)):
-        arr.append(sum)
-        sum += d
-    dic = dict(zip(arr,included))
-    for key, values in dic.items():
-        if values == True:
-            answer+=key
+    for idx, flag in enumerate(included):
+        if flag == True:
+            answer += a+idx*d
     return answer
