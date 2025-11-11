e32594c6c73e70bc1e03d6e1b141ac52bbf33685
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-11-11 15:26:36 +0900

    [level 0] Title: 배열 만들기 5, Time: 4.91 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.py
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.py
@@ -1,13 +1,7 @@
 def solution(intStrs, k, s, l):
-    result = []
-    for i in intStrs:
-        num = int(i[s:s+l])  
-        if num > k:
-            result.append(num)
-    return result
-
-
-
-# s번 인덱스에서 시작하는 l길이를 잘라 정수로 변환
-# 정수의 값이 k보다 큰 값들을 담은 배열 
-
+    answer = []
+    for num in intStrs:
+        n = num[s:s+l]
+        if k < int(n):
+            answer.append(int(n))
+    return answer
