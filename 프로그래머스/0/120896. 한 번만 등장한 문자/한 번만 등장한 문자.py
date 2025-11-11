20338a7a2a3602e5add91ac0555b5cffe11005ec
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-11-11 21:01:41 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.01 ms, Memory: 9.05 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
@@ -1,11 +1,6 @@
 def solution(s):
-    arr = []
     answer = ''
-    for i in list(s):
-        arr.append(i)
-    arr.sort()
-    for i in arr:
-        if arr.count(i)<2:
-            answer+=i
+    for c in 'abcdefghijklmnopqrstuvwxyz':
+        if s.count(c)==1:
+            answer += c
     return answer
-            
