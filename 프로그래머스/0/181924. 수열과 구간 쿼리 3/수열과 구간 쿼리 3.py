996b8a28ff5d541e87efbd4cec85d01078b99a01
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-11-11 16:06:32 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.11 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
@@ -1,8 +1,4 @@
 def solution(arr, queries):
-    answer = []
-    for i in queries:
-        i,j = i
-        temp = arr[i]
-        arr[i]=arr[j]
-        arr[j]=temp
+    for i,j in queries:
+        arr[i],arr[j] = arr[j],arr[i]
     return arr
