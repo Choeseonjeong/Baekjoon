dd3e63a08a272235c1342a09af53d71e7d71c855
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-11-12 19:52:51 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
@@ -1,7 +1,7 @@
 def solution(id_pw, db):
-    for i in db:
-        if id_pw[0] in i:
-            if id_pw[1] == i[1]:
+    for id, pw in db:
+        if id == id_pw[0]:
+            if pw ==id_pw[1]:
                 return "login"
             else:
                 return "wrong pw"
