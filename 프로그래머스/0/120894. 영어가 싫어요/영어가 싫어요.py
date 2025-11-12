7933510a4c146f8e7ef76f67ff1b751f4dea4bf4
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-11-12 16:17:25 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.04 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -1,19 +1,5 @@
 def solution(numbers):
-    nums = [
-        "zero",
-        "one",
-        "two",
-        "three",
-        "four",
-        "five",
-        "six",
-        "seven",
-        "eight",
-        "nine"
-    ]
-
-
-    for i, num in enumerate(nums):
-        numbers = numbers.replace(num,str(i))
-
+    arr = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    for idx, word in enumerate(arr):
+        numbers = numbers.replace(word, str(idx))
     return int(numbers)
