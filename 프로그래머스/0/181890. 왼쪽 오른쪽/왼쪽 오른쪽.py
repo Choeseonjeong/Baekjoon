b31830aa5cb2feac3ae02881141f399e788360e7
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-11-13 12:17:21 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.00 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
@@ -1,10 +1,7 @@
-# 먼저 나오는 l or r
-# l : 왼쪽 담기
-# r : 오른쪽 담기 
 def solution(str_list):
-    for idx, val in enumerate(str_list):
-        if val=="l":
+    for idx,word in enumerate(str_list):
+        if word == "l":
             return str_list[:idx]
-        if val=="r":
+        elif word == "r":
             return str_list[idx+1:]
     return []
