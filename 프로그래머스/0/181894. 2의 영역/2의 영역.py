7c3e2a6479e922600d55c5be3bb511db85884d3e
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-11-13 12:28:38 +0900

    [level 0] Title: 2의 영역, Time: 5.19 ms, Memory: 12.5 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.py
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.py
@@ -1,4 +1,11 @@
-def solution(arr):    
-    if 2 not in arr:        
-        return [-1]    
-    return arr[arr.index(2) : len(arr) - arr[::-1].index(2)]
+def solution(arr):
+    answer = []
+    f, b = 0, len(arr)-1
+    while f < b:
+        if arr[f] != 2:
+            f += 1
+        if arr[b] != 2:
+            b -= 1
+        if arr[f] == 2 and arr[b] == 2:
+            return arr[f:b+1]
+    return [-1]
