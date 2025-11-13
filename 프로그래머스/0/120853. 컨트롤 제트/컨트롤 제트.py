7ede579f03dac779d73ba33154a049ea231ae988
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-11-13 19:56:54 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.04 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.py
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.py
@@ -1,8 +1,10 @@
 def solution(s):
-    answer = []
-    for i in s.split():
-        if i == "Z":
-            answer.pop()
+    answer = 0
+    ex = 0
+    for num in s.split(" "):
+        if num != "Z":
+            ex = int(num) 
+            answer += int(num)
         else:
-            answer.append(int(i))
-    return sum(answer)
+            answer -= ex
+    return answer
