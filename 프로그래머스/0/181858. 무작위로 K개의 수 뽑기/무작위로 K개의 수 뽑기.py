422681a9986ac8aa65a3e5d9646436515559cb0d
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-11-15 15:38:43 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 62.19 ms, Memory: 9.72 MB -BaekjoonHub

--- 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.py
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.py
@@ -1,11 +1,10 @@
 def solution(arr, k):
-    ret = []
+    answer = []
     for i in arr:
-        if i not in ret:
-            ret.append(i)
-        if len(ret) == k:
+        if i not in answer:
+            answer.append(i)
+        if len(answer) >= k:
             break
-
-    return ret + [-1] * (k - len(ret))
-
-# k len 까지 지금까지 나온적이 없는 수이면 배열 맨 뒤에 추가하는 방식
+    while len(answer) < k:
+        answer += [-1]
+    return answer
