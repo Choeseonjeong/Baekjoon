d3f2bcdfc628b34d93a74dc389cb73857e6855f0
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-11-26 16:57:47 +0900

    [level 0] Title: 등수 매기기, Time: 0.01 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
@@ -1,7 +1,5 @@
 def solution(score):
-    people = [sum(i)/2 for i in score]
-    student = sorted(people, reverse=True)
-    result = []
-    for i in people:
-        result.append(student.index(i)+1)
-    return result
+    avg = [(a+b)/2 for a, b in score]
+    rank = sorted(avg, reverse=True)
+    return [rank.index(x) + 1 for x in avg]
+
