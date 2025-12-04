882357e6a491ee13da76160fd3fb8ccd2c3e7428
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-12-04 18:43:26 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.00 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
@@ -1,6 +1,6 @@
 def solution(array, height):
     answer = 0
-    for people in array:
-        if people > height:
+    for n in array:
+        if n > height:
             answer+=1
     return answer
