dc0444a9acd1aac966041e8446fd9eb8e0dd829c
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-12-04 18:14:47 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.01 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.py
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.py
@@ -1,6 +1,3 @@
 def solution(n, numlist):
     answer = []
-    for num in numlist:
-        if num%n==0:
-            answer.append(num)
-    return answer
+    return [num for num in numlist if num%n == 0 ]
