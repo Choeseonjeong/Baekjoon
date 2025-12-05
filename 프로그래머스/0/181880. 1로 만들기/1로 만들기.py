969f9733da5b41bc71fb4057f912fe56c569f1b9
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-12-05 21:00:44 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
@@ -2,9 +2,10 @@ def solution(num_list):
     answer = 0
     for num in num_list:
         while num != 1:
-            answer += 1
+            answer+=1
             if num%2==0:
-                num = num//2
+                num//=2
             else:
-                num = (num-1)//2
+                num-=1
+                num//=2
     return answer
