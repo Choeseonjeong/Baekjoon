fee6d7469d4846f12981935fedda8197ac1b665d
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-12-05 20:52:34 +0900

    [level 0] Title: 배열 만들기 5, Time: 3.42 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.py
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.py
@@ -2,6 +2,6 @@ def solution(intStrs, k, s, l):
     answer = []
     for num in intStrs:
         n = num[s:s+l]
-        if k < int(n):
+        if int(n) > k:
             answer.append(int(n))
     return answer
