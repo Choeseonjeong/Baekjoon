fbdf166bae6db1732a36d50d900cd118a7803532
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2025-12-15 15:41:13 +0900

    [level 0] Title: 합성수 찾기, Time: 0.51 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -1,11 +1,10 @@
 def solution(n):
-    answer = 0
-    for i in range(n+1):
-        c = 0
-        for j in range(1, i+1):
-            if i % j == 0:
-                c += 1
-        if c >= 3:
-            answer += 1
-        
-    return answer
+    num = []
+    count = 0
+    for i in range(2,n+1):
+        for j in range(1,i+1):
+            if i % j == 0 :
+                num.append(i)
+        if num.count(i) >=3:
+            count += 1
+    return count
