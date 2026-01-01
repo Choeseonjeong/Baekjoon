de4723c7974d6b1c40c29eef2aad30f8eb998815
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2026-01-01 21:44:12 +0900

    [level 0] Title: k의 개수, Time: 106.87 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.py
+++ 프로그래머스/0/120887. k의 개수/k의 개수.py
@@ -1,7 +1,8 @@
 def solution(i, j, k):
     answer = 0
-    for num in range(i,j+1):
-        for n in list(str(num)):
+    arr = [str(num) for num in range(i,j+1)]
+    for word in arr:
+        for n in word:
             if n == str(k):
-                answer += 1
+                answer+=1
     return answer
