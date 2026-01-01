024aa4f923eaf79a09bf4a02088a6d377d79add3
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2026-01-01 21:41:51 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,11 +1,10 @@
 def solution(rsp):
     answer = ''
-    # 가위는 2 / 바위는 0 / 보는 5
     for word in rsp:
-        if word=="2":
-            answer += "0"
-        elif word=="0":
-            answer+="5"
+        if word == "2":
+            answer+="0"
+        elif word == "0":
+            answer+='5'
         else:
-            answer+="2"
+            answer+='2'
     return answer
