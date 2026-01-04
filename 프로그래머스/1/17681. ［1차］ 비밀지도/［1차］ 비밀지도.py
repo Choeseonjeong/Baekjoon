0eec08e0044d6b942dcbd8dbf2482ccc2c8a954b
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2026-01-04 18:24:20 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.02 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,14 +1,8 @@
 def solution(n, arr1, arr2):
     answer = []
-    for idx,(a,b) in enumerate(zip(arr1,arr2)):
-        ar1 = bin(a)[2:] if len(bin(a)[2:]) == n else (n-len(bin(a)[2:]))*str(0)+bin(a)[2:]
-        ar2 = bin(b)[2:] if len(bin(b)[2:]) == n else (n-len(bin(b)[2:]))*str(0)+bin(b)[2:]
-        row = ''
-    
-        for i,j in zip(ar1,ar2):
-            if i == "1" or j == "1": 
-                row+="#"
-            else:
-                row+= " "
-        answer.append(row)
+    for idx in range(n):
+        num = bin(arr1[idx] | arr2[idx])
+        num = num[2:].zfill(n)
+        num = num.replace('1',"#").replace('0'," ")
+        answer.append(num)
     return answer
