19a36527a23723cf52fba9a71e1a6a7114e2ecfb
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2026-01-06 21:25:29 +0900

    [level 1] Title: 과일 장수, Time: 133.51 ms, Memory: 39.6 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -1,6 +1,7 @@
+import math
 def solution(k, m, score):
-    score.sort(reverse=True)
-    answer = 0
-    for i in range(0,len(score)//m*m,m):
-         answer += score[i + m - 1] * m
-    return answer
+    score = sorted(score)
+    n = math.ceil(len(score)/m)
+    while len(score) != n * m:
+        score = [0] + score
+    return sum([i*m for i in score[0::m]])
