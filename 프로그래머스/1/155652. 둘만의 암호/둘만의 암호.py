0e5a57e10cd4033fc6880c289a4e29c19bc7db8f
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2026-01-06 22:56:18 +0900

    [level 1] Title: 둘만의 암호, Time: 0.03 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
@@ -1,13 +1,9 @@
 def solution(s, skip, index):
-    arr = 'abcdefghijklmnopqrstuvwxyz'
-    answer =''
-    arr2 = []
-    for i in list(arr):
-        if i not in skip:
-            arr2.append(i)
+    answer = 'abcdefghijklmnopqrstuvwxyz'
+    result = ''
+    for ch in list(skip):
+        answer = answer.replace(ch,"")
             
-    for j in s:
-        idx = (arr2.index(j)+index)%len(arr2)
-        answer += arr2[idx]
-
-    return answer
+    for a in s:
+        result += answer[(answer.find(a) + index) % len(answer)]
+    return result
