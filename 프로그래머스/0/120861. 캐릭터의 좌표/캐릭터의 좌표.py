d144ea617cb1c94947019c28f54c9d3af8b426d2
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2026-02-12 01:31:33 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.02 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -1,12 +1,10 @@
 def solution(keyinput, board):
-    answer = {"up":[0,1],"down":[0,-1],"left":[-1,0],"right":[1,0]}
-    max_x, max_y = board[0]//2, board[1]//2
+    dir = {'up' : [0, 1], 'down' : [0, -1], 'left' : [-1, 0], 'right' : [1, 0]}
     x,y = 0,0
-    
-    for word in keyinput:
-        dx,dy = answer[word]
-        if abs(dx+x) > max_x or abs(dy+y) > max_y:
-            continue
-        else:
-            x,y = dx+x,dy+y
-    return [x,y]
+    m,n = board[0]//2,board[1]//2
+    for key in keyinput:
+        dx,dy = dir[key]
+        nx,ny = x+dx,y+dy
+        if -m <= nx <= m and -n <= ny <= n:
+            x, y = nx, ny   
+    return x,y
