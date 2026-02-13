78015f09a99dbc95b24666ef4d1677f08fc68623
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2026-02-14 00:43:21 +0900

    [level 0] Title: 안전지대, Time: 0.14 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.py
+++ 프로그래머스/0/120866. 안전지대/안전지대.py
@@ -1,23 +1,23 @@
 def solution(board):
-    m,n = len(board),len(board[0])
-    dx=[-1,1,0,0,-1,-1,1,1] 
-    dy=[0,0,-1,1,-1,1,-1,1]
-    boom = []
+    N = len(board)
+    dx = [-1, 1, 0, 0, -1, -1, 1, 1]
+    dy = [0, 0, -1, 1, -1, 1, -1, 1]
     
-    for i in range(m):
-        for j in range(n):
+    boom = []
+    for i in range(N):
+        for j in range(N):
             if board[i][j] == 1:
                 boom.append((i,j))
-    
+                
     for x,y in boom:
         for i in range(8):
-            nx = dx[i] + x
-            ny = dy[i] + y
-            if 0 <= nx < n and 0 <= ny < n:
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < N and 0 <= ny < N:
                 board[nx][ny] = 1
     count = 0
-    for i in range(m):
-        for j in range(n):
-            if board[i][j] == 0:
+    for x in range(N):
+        for y in range(N):
+            if board[x][y] == 0:
                 count += 1
     return count
+    
