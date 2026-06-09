a63303be250c1589c59ad6645b72128950376a45
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2026-06-09 16:39:13 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.53 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 9.2 MB, 시간: 10.59 ms
+메모리: 11.4 MB, 시간: 7.53 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 04일 17:13:35
+2026년 06월 09일 16:39:11
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,31 +1,26 @@
 from collections import deque
+
 def solution(maps):
-    n,m = len(maps),len(maps[0])
-    start = 0,0
+    M,N = len(maps),len(maps[0])
+    visited = [[-1]*N for _ in range(M)]
+    dx,dy = (-1,1,0,0),(0,0,-1,1)
     
-    dx = [-1,1,0,0]
-    dy = [0,0,-1,1]
+    q = deque()
+    q.append((0,0))
+    visited[0][0] = 1
     
-    queue = deque([start])
     
-    while queue:
-        x,y = queue.popleft()
+    while q:
+        x,y = q.popleft()
         for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
-            if 0<=nx<n and 0<=ny<m:
-                if maps[nx][ny] == 1:
-                    maps[nx][ny] = maps[x][y] + 1
-                    queue.append((nx,ny))
-                    
-                    
-    return maps[n-1][m-1] if maps[n-1][m-1] != 1 else -1
-            
-            
-            
-            
-            
-            
-            
-            
-            
+            nx,ny = x+dx[i],y+dy[i]
+            if 0<= nx < M and 0<= ny <N:
+                if maps[nx][ny] ==1 and visited[nx][ny] ==-1:
+                    visited[nx][ny] = visited[x][y] + 1
+                    q.append((nx,ny))
+                
+    return visited[M-1][N-1]
+                
+                
+                
+                
