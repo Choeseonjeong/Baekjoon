667023beb6380b39a1b94c859d91120b48569566
Choeseonjeong <128480236+Choeseonjeong@users.noreply.github.com>
2026-06-23 17:50:12 +0900

    [level 2] Title: 숫자 변환하기, Time: 215.64 ms, Memory: 18.9 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -1,18 +1,13 @@
-from collections import deque
-from collections import deque
-
-def solution(x, y, n):
-    queue = deque()
-    queue.append((x,0))
-    visited = set()
-    while queue:
-        i, j = queue.popleft()
-        if i>y or i in visited:
+def solution(x, y, n): 
+    dp = [1000000] * (y+1)
+    dp[x] = 0
+    for i in range(x,y+1):
+        if dp[i] == 1000000:
             continue
-        visited.add(i)
-        if i==y: 
-            return j
-        for k in (i*3,i*2,i+n):
-            if k<=y and k not in visited:
-                queue.append((k,j+1))    
-    return -1
+        if i+n <= y:
+            dp[i+n] = min(dp[i+n], dp[i] + 1)
+        if i*2 <= y:
+            dp[i*2] = min(dp[i*2], dp[i] + 1)
+        if i*3 <= y:
+            dp[i*3] = min(dp[i*3], dp[i] + 1)
+    return dp[y] if dp[y] != 1000000 else -1
